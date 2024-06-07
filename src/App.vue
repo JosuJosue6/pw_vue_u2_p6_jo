f4e85f2c879159c198fc050b8ab99f734ebabf54
Sebas <josuensd@gmail.com>
2024-06-06 19:50:21 -0500

    Taller 17: componentes y props

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-<Contador></Contador>
-
+<img src="./assets/logo.png" alt="No esta la imagen :c">
+  <Contador titulo="Estudiante"/><!--Cada uno es una isntancia diferente e independiente-->
+  <Contador titulo="Profesor"/>
+  <Contador/>
 </template>
 
 <script>
-import Contador from "./components/Contador.vue"
-
+import Contador from "./components/Contador.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Contador,
-    
-  }
-}
+  },
+};
 </script>
 
 <style>
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,16 +1,95 @@
 <template>
-    <h2>Contador</h2>
-    <p>100</p>
+  <h2>{{presentar}}</h2>
+  <p>{{ numero }} <sup>2</sup>= {{ calcular }}</p>
 
-  
+  <div>
+    <button @click="incrementar">+1</button>
+    <button @click="disminuir">-1</button>
+  </div>
 </template>
 
 <script>
+//Las propiedades re
 export default {
+  props: ["titulo","num"],
 
-}
+  data() {
+    //Opcion de Option API
+    return {
+      //proiedad reactiva
+      numero: 5,
+    };
+  },
+
+  methods: {
+    calcularCuadrado() {
+      console.log("Ingreso al metodo");
+      //Recordar siempre poner la palabra this.
+      return this.numero * this.numero;
+    },
+
+    calcularCuadrado2(num1, num2) {
+      console.log("Ingreso al metodo");
+      return num1 * num2;
+    },
+
+    incrementar() {
+      this.numero++;
+    },
+
+    disminuir() {
+      this.numero--;
+    },
+  },
+
+  //Propiedades computadas (son similares a los metodos pero no son metodos)
+  //como se maneja en cache es mucho mas eficiente como no es un metodo no recibira parametros
+
+  computed: {
+    calcular() {
+      //cadad uno es una propiedad computada
+      console.log("Ingreso a la propiedad computada");
+      return this.numero * this.numero;
+    },
+
+    presentar (){
+        if(this.titulo !== undefined){
+            return this.titulo;
+        }else{
+            return "texto que quiero"
+        }
+    }
+  },
+};
 </script>
 
 <style>
+/*h2 {
+  color: red;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-size: 80px;
+}*/
 
+button {
+  cursor: pointer;
+  border-radius: 5px;
+  border: 400px;
+  margin: 10px;
+  padding: 15px 40px;
+  background: rgb(22, 74, 87);
+  color: white;
+  font-size: 20px;
+}
+
+button:hover {
+  background: rgb(42, 150, 177);
+}
+
+p {
+  font-size: 25px;
+}
+
+h2 {
+  font-size: 30px;
+}
 </style>
